f7ca61c6f3d89dd188e290c5ae99bd4322fb6534
DasMaiv <151778611+DasMaiv@users.noreply.github.com>
2024-01-12 20:49:01 +0100

    Start

---
+++ logic/logic.js
@@ -0,0 +1,47 @@
+document.getElementById("Blast-X").style.display = "none";
+document.getElementById("Dose").style.display = "none";
+document.getElementById("Halterung").style.display = "none";
+
+function produktAnzeigen(nr) {
+    if(nr === 1) {
+        document.getElementById("RotPLA").style.display = "block";
+        document.getElementById("HellblauPLA").style.display = "block";
+        document.getElementById("GrünPLA").style.display = "block";
+        document.getElementById("SchwarzWeißPLA").style.display = "block";
+        document.getElementById("MarmorPLA").style.display = "block";
+        document.getElementById("WeißSilkPLA").style.display = "block";
+        document.getElementById("SilberSilkPLA").style.display = "block";
+        document.getElementById("SchwarzSilkPLA").style.display = "block";
+        document.getElementById("StahlblauSilkPLA").style.display = "block";
+        document.getElementById("LilaSilkPLA").style.display = "block";
+        document.getElementById("PinkSilkPLA").style.display = "block";
+        document.getElementById("GrünSilkPLA").style.display = "block";
+        document.getElementById("BronzeSilkPLA").style.display = "block";
+        document.getElementById("KupferSilkPLA").style.display = "block";
+        document.getElementById("GoldSilkPLA").style.display = "block";
+        document.getElementById("Blast-X").style.display = "none";
+        document.getElementById("Dose").style.display = "none";
+        document.getElementById("Halterung").style.display = "none";
+    }
+
+    if(nr === 2) {
+        document.getElementById("RotPLA").style.display = "none";
+        document.getElementById("HellblauPLA").style.display = "none";
+        document.getElementById("GrünPLA").style.display = "none";
+        document.getElementById("SchwarzWeißPLA").style.display = "none";
+        document.getElementById("MarmorPLA").style.display = "none";
+        document.getElementById("WeißSilkPLA").style.display = "none";
+        document.getElementById("SilberSilkPLA").style.display = "none";
+        document.getElementById("SchwarzSilkPLA").style.display = "none";
+        document.getElementById("StahlblauSilkPLA").style.display = "none";
+        document.getElementById("LilaSilkPLA").style.display = "none";
+        document.getElementById("PinkSilkPLA").style.display = "none";
+        document.getElementById("GrünSilkPLA").style.display = "none";
+        document.getElementById("BronzeSilkPLA").style.display = "none";
+        document.getElementById("KupferSilkPLA").style.display = "none";
+        document.getElementById("GoldSilkPLA").style.display = "none";
+        document.getElementById("Blast-X").style.display = "block";
+        document.getElementById("Dose").style.display = "block";
+        document.getElementById("Halterung").style.display = "block";
+    }
+}
