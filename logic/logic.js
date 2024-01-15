4365d1da541e5abf64c68df64d93d07f6e6f74b2
DasMaiv <151778611+DasMaiv@users.noreply.github.com>
2024-01-15 21:20:41 +0100

    Update!!
    Diesmal wurden Preise, die neue Filament-Spardose und der neue Tab "Einhörner" hinzugefügt, für alle Einhornfans.

--- logic/logic.js
+++ logic/logic.js
@@ -1,9 +1,31 @@
+document.getElementById("Willkommenstext").style.display = "block";
+document.getElementById("RotPLA").style.display = "none";
+document.getElementById("HellblauPLA").style.display = "none";
+document.getElementById("GrünPLA").style.display = "none";
+document.getElementById("SchwarzWeißPLA").style.display = "none";
+document.getElementById("MarmorPLA").style.display = "none";
+document.getElementById("WeißSilkPLA").style.display = "none";
+document.getElementById("SilberSilkPLA").style.display = "none";
+document.getElementById("SchwarzSilkPLA").style.display = "none";
+document.getElementById("StahlblauSilkPLA").style.display = "none";
+document.getElementById("LilaSilkPLA").style.display = "none";
+document.getElementById("PinkSilkPLA").style.display = "none";
+document.getElementById("GrünSilkPLA").style.display = "none";
+document.getElementById("BronzeSilkPLA").style.display = "none";
+document.getElementById("KupferSilkPLA").style.display = "none";
+document.getElementById("GoldSilkPLA").style.display = "none";
 document.getElementById("Blast-X").style.display = "none";
 document.getElementById("Dose").style.display = "none";
 document.getElementById("Halterung").style.display = "none";
+document.getElementById("Spardose").style.display = "none";
+document.getElementById("SpardosenHalterung").style.display = "none";
+document.getElementById("Glückshorn").style.display = "none";
+document.getElementById("Sparhorn").style.display = "none";
+
 
 function produktAnzeigen(nr) {
     if(nr === 1) {
+        document.getElementById("Willkommenstext").style.display = "none";
         document.getElementById("RotPLA").style.display = "block";
         document.getElementById("HellblauPLA").style.display = "block";
         document.getElementById("GrünPLA").style.display = "block";
@@ -22,9 +44,14 @@ function produktAnzeigen(nr) {
         document.getElementById("Blast-X").style.display = "none";
         document.getElementById("Dose").style.display = "none";
         document.getElementById("Halterung").style.display = "none";
+        document.getElementById("Spardose").style.display = "none";
+        document.getElementById("SpardosenHalterung").style.display = "none";
+        document.getElementById("Glückshorn").style.display = "none";
+        document.getElementById("Sparhorn").style.display = "none";
     }
 
     if(nr === 2) {
+        document.getElementById("Willkommenstext").style.display = "none";
         document.getElementById("RotPLA").style.display = "none";
         document.getElementById("HellblauPLA").style.display = "none";
         document.getElementById("GrünPLA").style.display = "none";
@@ -43,5 +70,35 @@ function produktAnzeigen(nr) {
         document.getElementById("Blast-X").style.display = "block";
         document.getElementById("Dose").style.display = "block";
         document.getElementById("Halterung").style.display = "block";
+        document.getElementById("Spardose").style.display = "block";
+        document.getElementById("SpardosenHalterung").style.display = "block";
+        document.getElementById("Glückshorn").style.display = "none";
+        document.getElementById("Sparhorn").style.display = "none";
+    }
+
+    if(nr === 3) {
+        document.getElementById("Willkommenstext").style.display = "none";
+        document.getElementById("RotPLA").style.display = "none";
+        document.getElementById("HellblauPLA").style.display = "none";
+        document.getElementById("GrünPLA").style.display = "none";
+        document.getElementById("SchwarzWeißPLA").style.display = "none";
+        document.getElementById("MarmorPLA").style.display = "none";
+        document.getElementById("WeißSilkPLA").style.display = "none";
+        document.getElementById("SilberSilkPLA").style.display = "none";
+        document.getElementById("SchwarzSilkPLA").style.display = "none";
+        document.getElementById("StahlblauSilkPLA").style.display = "none";
+        document.getElementById("LilaSilkPLA").style.display = "none";
+        document.getElementById("PinkSilkPLA").style.display = "none";
+        document.getElementById("GrünSilkPLA").style.display = "none";
+        document.getElementById("BronzeSilkPLA").style.display = "none";
+        document.getElementById("KupferSilkPLA").style.display = "none";
+        document.getElementById("GoldSilkPLA").style.display = "none";
+        document.getElementById("Blast-X").style.display = "none";
+        document.getElementById("Dose").style.display = "none";
+        document.getElementById("Halterung").style.display = "none";
+        document.getElementById("Spardose").style.display = "none";
+        document.getElementById("SpardosenHalterung").style.display = "none";
+        document.getElementById("Glückshorn").style.display = "block";
+        document.getElementById("Sparhorn").style.display = "block";
     }
 }
